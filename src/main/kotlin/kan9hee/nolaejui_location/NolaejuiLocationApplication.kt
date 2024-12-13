bc8fdc6361fbebb7506ec2992f7a7f2b1d71359d
han <hcsy1219@gmail.com>
2024-12-13 10:01:40 +0900

    Init: Upload location microservice project

---
+++ src/main/kotlin/kan9hee/nolaejui_location/NolaejuiLocationApplication.kt
@@ -0,0 +1,11 @@
+package kan9hee.nolaejui_location
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NolaejuiLocationApplication
+
+fun main(args: Array<String>) {
+	runApplication<NolaejuiLocationApplication>(*args)
+}
---
+++ src/test/kotlin/kan9hee/nolaejui_location/NolaejuiLocationApplicationTests.kt
@@ -0,0 +1,13 @@
+package kan9hee.nolaejui_location
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NolaejuiLocationApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}

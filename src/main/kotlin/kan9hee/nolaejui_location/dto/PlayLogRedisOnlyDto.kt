a554a624b7449fc3579ccef3801723b40fb9599e
han <hcsy1219@gmail.com>
2025-05-05 19:08:39 +0900

    refactor: Remove PlayLogRedisOnlyDto after Redis logic update

--- src/main/kotlin/kan9hee/nolaejui_location/dto/PlayLogRedisOnlyDto.kt
+++
@@ -1,5 +0,0 @@
-package kan9hee.nolaejui_location.dto
-
-data class PlayLogRedisOnlyDto(val musicId:Long,
-                               val userInfo:String,
-                               val timestamp:Long)

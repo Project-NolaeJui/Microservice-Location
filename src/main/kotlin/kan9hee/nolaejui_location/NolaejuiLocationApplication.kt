2e550ea50bdfe74b04ee0913cf51cc2f77458a9c
han <hcsy1219@gmail.com>
2025-05-05 19:12:22 +0900

    fix: Fix missing scheduling annotation

--- src/main/kotlin/kan9hee/nolaejui_location/NolaejuiLocationApplication.kt
+++ src/main/kotlin/kan9hee/nolaejui_location/NolaejuiLocationApplication.kt
@@ -3,7 +3,9 @@ package kan9hee.nolaejui_location
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient
+import org.springframework.scheduling.annotation.EnableScheduling
 
+@EnableScheduling
 @SpringBootApplication
 @EnableDiscoveryClient
 class NolaejuiLocationApplication
